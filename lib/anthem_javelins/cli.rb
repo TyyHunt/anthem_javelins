c131d9f1e8e8796c982d282045a9307b897aee4f
Tyler Hunt <tyhunt04@gmail.com>
2019-03-08 21:08:09 +0000

    created menu class in cli.rb

--- lib/anthem_javelins/cli.rb
+++ lib/anthem_javelins/cli.rb
@@ -1,7 +1,19 @@
 class AnthemJavelins::CLI 
   
   def call
+    menu
+  end
+  
+  def menu
     puts "Want to know more about Anthem's javelins?"
+    puts "Here is a list of Anthem's current javelins"
+    puts <<-DOC
+      1. Hunter Javelin
+      2. Colossus Javelin
+      3. Storm Javelin
+      4. Interceptor Javelin
+      DOC
+      
   end
   
 end
