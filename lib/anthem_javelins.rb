01ac46b18cfe6e2e4990211d6cf2fc7f02aa4326
Tyler Hunt <tyhunt04@gmail.com>
2019-03-08 18:24:55 +0000

    first commit

---
+++ lib/anthem_javelins.rb
@@ -0,0 +1,6 @@
+require "anthem_javelins/version"
+
+module AnthemJavelins
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/anthem_javelins/version.rb
@@ -0,0 +1,3 @@
+module AnthemJavelins
+  VERSION = "0.1.0"
+end
