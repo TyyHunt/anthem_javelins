1c8d093c6027b63a44b76f90a4a867b37ad204d4
Tyler Hunt <tyhunt04@gmail.com>
2019-03-09 16:52:37 +0000

    added gems Nokogiri and Pry

--- lib/anthem_javelins.rb
+++ lib/anthem_javelins.rb
@@ -1,3 +1,7 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
 require_relative "anthem_javelins/version"
 require_relative "./anthem_javelins/cli"
 require_relative "./anthem_javelins/javelins"
