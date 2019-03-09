144d48d10469b1c90ea0bf2dfda62e1f486dfd47
Tyler Hunt <tyhunt04@gmail.com>
2019-03-09 18:05:06 +0000

    duplicated scrape_hunter for other javelins

--- lib/anthem_javelins/javelins.rb
+++ lib/anthem_javelins/javelins.rb
@@ -31,11 +31,50 @@ class AnthemJavelins::Javelins
     javelins = []
     
     javelins << self.scrape_ranger
+    javelins << self.scrape_colossus
+    javelins << self.scrape_storm
+    javelins << self.scrape_interceptor
   end
   
   def self.scrape_ranger
     
     doc = Nokogiri::HTML(open("https://www.ea.com/games/anthem/gameplay-features/ranger-javelin"))
+    
+    name = doc.search("h1.d2").text
+    description = doc.css("p")[0].text
+    fighting_style = doc.search("ea-details-table").text
+    special_1 = doc.css("h4")[0].text
+    special_2 = doc.css("h4")[1].text
+    binding.pry 
+  end
+  
+  def self.scrape_colossus
+    
+    doc = Nokogiri::HTML(open("https://www.ea.com/games/anthem/gameplay-features/colossus-javelin"))
+    
+    name = doc.search("h1.d2").text
+    description = doc.css("p")[0].text
+    fighting_style = doc.search("ea-details-table").text
+    melee = doc.css("h4")[0].text
+    ult = doc.css("h4")[1].text
+    binding.pry 
+  end
+  
+  def self.scrape_storm
+    
+    doc = Nokogiri::HTML(open("https://www.ea.com/games/anthem/gameplay-features/storm-javelin"))
+    
+    name = doc.search("h1.d2").text
+    description = doc.css("p")[0].text
+    fighting_style = doc.search("ea-details-table").text
+    melee = doc.css("h4")[0].text
+    ult = doc.css("h4")[1].text
+    binding.pry 
+  end
+  
+  def self.scrape_interceptor
+    
+    doc = Nokogiri::HTML(open("https://www.ea.com/games/anthem/gameplay-features/interceptor-javelin"))
     
     name = doc.search("h1.d2").text
     description = doc.css("p")[0].text
