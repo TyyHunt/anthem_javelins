33a63cb7da6fdcc6df1f2f38defaf9ab6174bb8b
Tyler Hunt <tyhunt04@gmail.com>
2019-03-11 19:21:09 +0000

    finalized additional info section for other javelins

--- lib/anthem_javelins/cli.rb
+++ lib/anthem_javelins/cli.rb
@@ -36,11 +36,29 @@ class AnthemJavelins::CLI
         puts "#{ranger_info.url}"
         puts " "
       when "colossus"
-        puts "info about Colossus"
+        colossus_info = @javelins[1]
+        puts "#{colossus_info.description}"
+        puts "#{colossus_info.fighting_style_pairs}"
+        puts "Speceial Ability - #{colossus_info.special_1}"
+        puts "Speceial Ability - #{colossus_info.special_2}"
+        puts "#{colossus_info.url}"
+        puts " "
       when "storm"
-        puts "info about Storm"
+        storm_info = @javelins[2]
+        puts "#{storm_info.description}"
+        puts "#{storm_info.fighting_style_pairs}"
+        puts "Speceial Ability - #{storm_info.special_1}"
+        puts "Speceial Ability - #{storm_info.special_2}"
+        puts "#{storm_info.url}"
+        puts " "
       when "interceptor"
-        puts "info about Interceptor"
+         interceptor_info = @javelins[3]
+        puts "#{interceptor_info.description}"
+        puts "#{interceptor_info.fighting_style_pairs}"
+        puts "Speceial Ability - #{interceptor_info.special_1}"
+        puts "Speceial Ability - #{interceptor_info.special_2}"
+        puts "#{interceptor_info.url}"
+        puts " "
       when "list"
         list_javelins
       end
