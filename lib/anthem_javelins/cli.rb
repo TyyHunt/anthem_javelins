f4a1b9f9cb8651357ff1018b9d88c9499038bfba
Tyler Hunt <tyhunt04@gmail.com>
2019-03-11 19:25:24 +0000

    user interface fixes

--- lib/anthem_javelins/cli.rb
+++ lib/anthem_javelins/cli.rb
@@ -28,39 +28,49 @@ class AnthemJavelins::CLI
     input= gets.strip.downcase
       case input
       when "ranger"
+        puts "\n"
         ranger_info = @javelins[0]
         puts "#{ranger_info.description}"
         puts "#{ranger_info.fighting_style_pairs}"
         puts "Speceial Ability - #{ranger_info.special_1}"
         puts "Speceial Ability - #{ranger_info.special_2}"
         puts "#{ranger_info.url}"
-        puts " "
+        puts "\n"
+        
       when "colossus"
+        puts "\n"
         colossus_info = @javelins[1]
         puts "#{colossus_info.description}"
         puts "#{colossus_info.fighting_style_pairs}"
         puts "Speceial Ability - #{colossus_info.special_1}"
         puts "Speceial Ability - #{colossus_info.special_2}"
         puts "#{colossus_info.url}"
-        puts " "
+        puts "\n"
+        
       when "storm"
+        puts "\n"
         storm_info = @javelins[2]
         puts "#{storm_info.description}"
         puts "#{storm_info.fighting_style_pairs}"
         puts "Speceial Ability - #{storm_info.special_1}"
         puts "Speceial Ability - #{storm_info.special_2}"
         puts "#{storm_info.url}"
-        puts " "
+        puts "\n"
+        
       when "interceptor"
+        puts "\n"
          interceptor_info = @javelins[3]
         puts "#{interceptor_info.description}"
         puts "#{interceptor_info.fighting_style_pairs}"
         puts "Speceial Ability - #{interceptor_info.special_1}"
         puts "Speceial Ability - #{interceptor_info.special_2}"
         puts "#{interceptor_info.url}"
-        puts " "
+        puts "\n"
+        
       when "list"
         list_javelins
+      else
+        puts "sorry that is not a valid input \n"
       end
     end
   end
